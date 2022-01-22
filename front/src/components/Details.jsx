01a6fcf20ffcfe783fa19004f1d652cb2f2f5a6a
Facundo Sanchez <sanchezfacundocristian@hotmail.com>
2022-01-22 03:12:08 -0300

    adding responsive and new styles

--- front/src/components/Details.jsx
+++ front/src/components/Details.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { getDetails } from '../redux/actions'
 import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import Navbar from './Navbar'
 import '../styles/Details.css'
 
 function Details() {
@@ -17,19 +18,25 @@ function Details() {
     },[id, dispatch])
     
     return (
-        <div>
-            <h1>Ruta detalles</h1>
-            <a href="/home"><button>Volver a inicio</button></a>
+        <div className='container__details__route'>
+            <nav>
+                <Navbar/>
+            </nav>
+            <a href="/home"><button className='btn__inicio' >Inicio</button></a>
             {
                 product ? product.map(el => {
                     return (
-                        <div key={el.id}>
-                            <img src={el.img} alt="Imagen no encontrada" height="400px"/>
-                            <h2>{el.product}</h2>
-                            <h3>{el.price}</h3>
-                            <h3>{el.description}</h3>
-                            <a href={wpp} target='_blank' rel="noopener noreferrer" ><button>Comprar</button></a>
-                            <button>Contactar</button>
+                        <div className='container__details__items' key={el.id}>
+                            <div className='img__details'>
+                                <img src={el.img} alt="Imagen no encontrada" height="400px"/>
+                            </div>
+                            <div className='detail__items'>
+                                <h2>{el.product}</h2>
+                                <h3>${el.price}</h3>
+                                <h3>{el.description}</h3>
+                                <a href={wpp} target='_blank' rel="noopener noreferrer" ><button>Comprar</button></a>
+                                <button>Contactar</button>
+                            </div>
                         </div>
                     )
                 }) :
