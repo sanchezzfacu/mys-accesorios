11921dcb52bcac9d715970d85822027969c6249b
Facundo Sanchez <sanchezfacundocristian@hotmail.com>
2022-03-01 16:01:19 -0300

    cambios en footer

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -7,10 +7,12 @@ import '../styles/Footer.css'
 function Footer() {
     return (
             <footer>
-                <span>Seguinos en nuestras redes</span>
-                <a className='ig__item' href="https://www.instagram.com/_mys_accesorios/" target='_blank' rel='noreferrer'><img src={ig} alt='' height='60px'/></a>
-                <a className='fb__item' href="https://www.facebook.com/MyS_accesorios-103511165361163" target='_blank' rel='noreferrer'><img src={fb} alt='' height='60px'/></a>
-                <a className='wpp__item' href="https://wa.me/543876396788?text=Hola%20estoy%20interesada%20en%20uno%20de%20sus%20productos,%20me%20das%20informacion?" target='_blank' rel='noreferrer'><img src={wpp} alt='' height='60px'/></a>
+                <div className='footer-span'>
+                    <span>Seguinos en nuestras redes</span>
+                </div>
+                <a className='ig__item' href="https://www.instagram.com/_mys_accesorios/" target='_blank' rel='noreferrer'><img className='img-footer' src={ig} alt='' height='60px'/></a>
+                <a className='fb__item' href="https://www.facebook.com/MyS_accesorios-103511165361163" target='_blank' rel='noreferrer'><img className='img-footer' src={fb} alt='' height='60px'/></a>
+                <a className='wpp__item' href="https://wa.me/543876396788?text=Hola%20estoy%20interesada%20en%20uno%20de%20sus%20productos,%20me%20das%20informacion?" target='_blank' rel='noreferrer'><img className='img-footer' src={wpp} alt='' height='60px'/></a>
             </footer>
     )
 }
